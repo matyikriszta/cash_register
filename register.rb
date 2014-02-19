e23c2229bab431cc0c19c6332bbc2845b2094a69
Kriszta <matyi.kriszta@gmail.com>
2014-02-19 18:26:11 +0000

    added float functionality

--- register.rb
+++ register.rb
@@ -1,14 +1,25 @@
 class Register
 
-  def denominations
-    [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+  attr_accessor :denominations
+
+  def initialize(coins = {})
+    @denominations = coins
   end
 
-  def change_given(value)
+  def change_given(change)
     result = {}
-    denominations.each do |d|
-      result[d] = (value / d).floor
-      value = value % d
+    denominations.each do |coin_value, quantity|
+      coins_needed = (change / coin_value).floor
+      if quantity >= coins_needed
+        result[coin_value] = coins_needed
+        denominations[coin_value] -= coins_needed
+        change = change % coin_value
+      else
+        result[coin_value] = denominations[coin_value]
+        denominations[coin_value] = 0
+        qty_of_coins_needed = coins_needed - result[coin_value]
+        change = (change % coin_value) + (qty_of_coins_needed*coin_value)
+      end
     end
     result.reject { |k,v| v == 0 }
   end
--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -1,9 +1,10 @@
 require_relative 'spec_helper'
 require_relative '../register.rb'
+require 'pry-byebug'
 
 describe Register do
 
-  let(:register) { Register.new }
+  let(:register) { Register.new({2 => 4, 1 => 1}) }
 
   describe '.new' do
     it 'creates a new register' do
@@ -13,28 +14,34 @@ describe Register do
 
   describe '#denominations' do
     it 'should return the denominations' do
-      expect(register.denominations).to eql [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+      register = Register.new({2 => 4})
+      expect(register.denominations).to eql({2 => 4})
     end
   end
 
   describe "#change_given" do
     it 'should return 1 x £2 for £2' do
+      register = Register.new({2 => 4})
       expect(register.change_given(2)).to eql({2 => 1})
     end
 
     it 'should return 2 x £2 for £4' do
+      register = Register.new({2 => 4})
       expect(register.change_given(4)).to eql({2 => 2})
     end
 
     it 'should return 1 x £2, 1 x £1 for £3' do
+      register = Register.new({2 => 2, 1 => 1})
       expect(register.change_given(3)).to eql({2 => 1, 1 => 1})
     end
 
     it 'should return 1 x £2, 1 x £1, 1 x 50p for £3.50' do
+      register = Register.new({2 => 2, 1 => 1, 0.5 => 1})
       expect(register.change_given(3.50)).to eql({2 => 1, 1 => 1, 0.5 => 1})
     end
 
     it 'should return 1 x £2, 1 x £1, 1 x 50p, 1 x 20p for £3.70' do
+      register = Register.new({2 => 2, 1 => 1, 0.5 => 1, 0.2 => 1})
       expect(register.change_given(3.70)).to eql({2 => 1, 1 => 1, 0.5 => 1, 0.2 => 1})
     end
   end
@@ -45,11 +52,23 @@ describe Register do
     end
 
     it "should return the correct change denominations" do
+      register = Register.new({0.2 => 2, 0.05 => 1, 0.02 => 2})
       expect(register.transaction(4.51, 5)).to eql({0.2 => 2, 0.05 => 1, 0.02 => 2})
     end
 
   it "should return the correct change denominations" do
+      register = Register.new({2 => 2, 0.5 => 1, 0.1 => 1, 0.02 => 2})
       expect(register.transaction(15.36, 20)).to eql({2 => 2, 0.5 => 1, 0.1 => 1, 0.02 => 2})
+    end
+
+    it "should return the correct change with limited coins" do
+      register = Register.new({2 => 1, 1 => 2, 0.5 => 1, 0.1 => 1, 0.02 => 2})
+      expect(register.transaction(15.36, 20)).to eql({2 => 1, 1 => 2, 0.5 => 1, 0.1 => 1, 0.02 => 2})
+    end
+
+    it "should return the correct change with limited coins" do
+      register = Register.new({2 => 1, 1 => 1, 0.5 => 2, 0.1 => 1, 0.02 => 2})
+      expect(register.transaction(15.36, 20)).to eql({2 => 1, 1 => 1, 0.5 => 2, 0.1 => 1, 0.02 => 2})
     end
   end
 
